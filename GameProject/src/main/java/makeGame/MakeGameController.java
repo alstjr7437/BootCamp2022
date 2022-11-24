aaa63bbabbbfa2dc3cae8ff155de0c33827b64af
김민석|Minseok Kim <alstjr7437@naver.com>
2022-11-24 15:56:23 +0900

    테스트

--- GameProject/src/main/java/makeGame/MakeGameController.java
+++ GameProject/src/main/java/makeGame/MakeGameController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("*.ct")
 public class MakeGameController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
